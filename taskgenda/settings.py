8a3cbdcb8cafbd8c8ea83491e42462dc5de18fb0
Nourk4441 <nourk4441@gmail.com>
2025-01-04 23:41:41 +0200

    Fixing issues

--- taskgenda/settings.py
+++ taskgenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b!n^p(5c=8g@lqujwdo^lap2a13%p2#=3)h!8#$8$0-(7zyxf3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polar-waters-56773.herokuapp.com', 'localhost']
 
 
 # Application definition
